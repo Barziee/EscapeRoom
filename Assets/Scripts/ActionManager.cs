3667cbd57306c746ce6e277d678559d281b78d0d
Roza13-bit <roizamir876@gmail.com>
2020-12-26 18:31:47 +0200

    V0.1 - Room + Pickup

---
+++ Assets/Scripts/ActionManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionManager : MonoBehaviour
+{
+    public bool isKeyPicked = false;
+    public bool isDoorOpened = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDoorOpened)
+        {
+            Debug.Log("~~~ Jew Won!!! ~~~");
+
+        }
+
+        if (isKeyPicked)
+        {
+
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/PickupScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupScript : MonoBehaviour
+{
+    GameObject pickedObject;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    var tmpObj = hit.collider.gameObject;
+                    Destroy(tmpObj);
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
